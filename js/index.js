a37943b50b5441f470c6eff6ecb44d53f9c8a103
Lileah-ni <99857698+Lileah-ni@users.noreply.github.com>
2022-08-06 23:24:31 +1000

    Added

---
+++ js/index.js
@@ -0,0 +1,46 @@
+
+ScrollReveal().reveal('.navigation-bar');
+
+
+// GENERAL SETTING
+window.sr = ScrollReveal({ reset: true });
+
+// Custom Settings
+sr.reveal('.navigation-bar', { duration: 200 });
+
+sr.reveal('.banner', { 
+  origin: 'right', 
+  duration: 2000 
+});
+
+sr.reveal('#container', { 
+    origin: 'right', 
+    duration: 2000 
+  });
+
+sr.reveal('.skill-sec', { 
+    origin: 'right', 
+    duration: 2000 
+  });
+
+sr.reveal('#about-me', { 
+    origin: 'right', 
+    duration: 2000 
+  });
+
+
+
+sr.reveal('#portfolio', { 
+    origin: 'right', 
+    duration: 3000 
+  });
+
+sr.reveal('#grid-img', { 
+    origin: 'right', 
+  duration: 5000 
+});
+
+
+
+
+
